820c60ccb3193d77bd4915e26a1987b4618161a5
Camilo Zambrano <camilozambranovotto@hotmail.com>
2021-10-23 10:17:26 -0500

    Update message.zsh

--- .config/zsh/message.zsh
+++ .config/zsh/message.zsh
@@ -5,3 +5,5 @@ autoload colors; colors
 message=$(< $ZDOTDIR"/message.txt")
 echo $fg[red]$message
 echo
+echo $fg[yellow]"\t\t\tWelcome "${USER}" to Zsh"
+echo $fg[yellow]"\t\t    Good luck with your hacking! 😎"
