3b0740a90a9a78162e92f163dfa8c64d67bca76c
Camilo Zambrano <camilozambranovotto@hotmail.com>
2021-11-23 20:41:56 -0500

    refactor: makes the string creation more clear.

--- .config/zsh/message.zsh
+++ .config/zsh/message.zsh
@@ -12,5 +12,5 @@ autoload colors; colors
 message=$(< $ZDOTDIR"/message.txt")
 echo $fg[red]$message
 echo
-echo $fg[yellow]"\t\t\tWelcome "${USER}" to Zsh"
+echo $fg[yellow]"\t\t\tWelcome $USER to Zsh"
 echo $secondMsg
