6373fc6111e9e0039bbd17c6ca0085a729b56edb
Camilo Zambrano <camilozambranovotto@hotmail.com>
2022-04-16 23:33:38 -0500

    Update message.zsh

--- .config/zsh/message.zsh
+++ .config/zsh/message.zsh
@@ -2,15 +2,15 @@
 # Ascii art taken from:
 #       https://github.com/kadekillary/dotfiles/blob/master/nvim/init.vim
 
-secondMsg=("\t\t    Good luck with your hacking! 😎")
-secondMsg+=("\t\t     What are we hacking today?👨‍💻")
-sizeSecond=${#secondMsg[@]}
-random=$(shuf -i 1-$sizeSecond -n 1)
-secondMsg="${secondMsg[$random]}"
+secondMsgArr=("\t\t    Good luck with your hacking! 😎")
+secondMsgArr+=("\t\t     What are we hacking today?👨‍💻")
+sizeSecond=${#secondMsgArr[@]}
+random=$(shuf -i 1-"$sizeSecond" -n 1)
+secondMsg="${secondMsgArr[$random]}"
 
 autoload colors; colors
-message=$(< $ZDOTDIR"/message.txt")
-echo $fg[red]$message
+message="$(< "$ZDOTDIR/message.txt")"
+echo "${fg[red]}$message"
 echo
-echo $fg[yellow]"\t\t\tWelcome $USER to Zsh"
-echo $secondMsg
+echo "${fg[yellow]}\t\t\tWelcome $USER to Zsh"
+echo "$secondMsg"
